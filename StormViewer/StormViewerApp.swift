1b2019943e6bd709c9b935f5607450b2af4324d5
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-11-05 03:22:58 +0100

    Initial Commit

---
+++ StormViewer/StormViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StormViewerApp.swift
+//  StormViewer
+//
+//  Created by Zaid Neurothrone on 2022-11-05.
+//
+
+import SwiftUI
+
+@main
+struct StormViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
